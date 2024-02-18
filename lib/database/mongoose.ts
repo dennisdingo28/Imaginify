06d6ba015ed603795c664eaafa6215184a7ad373
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-02-18 13:00:59 +0200

    chore - create db schemas

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,3 +7,22 @@ interface MongooseConnection {
     promise: Promise<Mongoose> | null;
 }
 
+let cached: MongooseConnection = (global as any).mongoose;
+
+if(!cached) {
+    cached = (global as any).mongoose = {
+        conn: null, promise: null
+    };
+}
+
+export const connectToDatabase = async () =>{
+    if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {dbName:"Imaginify", bufferCommands:false});
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+}
